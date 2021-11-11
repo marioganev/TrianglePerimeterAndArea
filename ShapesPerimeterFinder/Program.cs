62fe0e47ce8f6e8b8075fde80e2cf7a171baa033
Aleks7391 <playtohack7@gmail.com>
2021-11-11 21:36:03 +0200

    Add project files.

---
+++ ShapesPerimeterFinder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ShapesPerimeterFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Triangle Perimeter & Area Finder");
+            Console.WriteLine("         by Mario Ganev");
+            Console.WriteLine("");
+            int a, b, c, h, p, s;
+            Console.Write("a = ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("b = ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("c = ");
+            c = int.Parse(Console.ReadLine());
+            Console.Write("h = ");
+            h = int.Parse(Console.ReadLine());
+            s = h * b / 2;
+            p = a + b + c;
+            Console.WriteLine("");
+            
+
+            Console.WriteLine("Perimeter = " + p);
+            Console.WriteLine("Area = " + s);
+
+        }
+    }
+}
